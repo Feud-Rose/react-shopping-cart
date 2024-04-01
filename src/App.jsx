6836f29d4c85ae65308032bb3b7fe811f55e42b0
Feud-Rose <145285546+Feud-Rose@users.noreply.github.com>
2024-04-01 10:55:32 -0600

    feat: setup Vite + React, and src sub folders

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [userCart, setUserCart] = useState({})
+
+  return (
+    <>
+      <p className="hello">
+        Hello
+      </p>
+    </>
+  )
+}
+
+export default App
